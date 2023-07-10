ff2f40c66faa0b5ed33213d187c750d99d421b51
SayuruD <d.sayuru@directfn.com>
2023-07-11 00:57:27 +0530

    Configuration added. without qulifier instsntiste customer repo

--- demo/src/main/java/com/example/demo/customer/CustomerConfig.java
+++ demo/src/main/java/com/example/demo/customer/CustomerConfig.java
@@ -1,16 +1,27 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CustomerConfig {
+
+    @Value("${app.fakeCustomerRepo:false}")
+    Boolean fakeCustomerRepo;
+
     @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {
             System.out.println("HEllo spring");
         };
+
+    }
+    @Bean
+    CustomerRepo customerRepo(){
+        System.out.println("useFakeCustomerRepo = "+ fakeCustomerRepo);
+        return fakeCustomerRepo ? new FakeCustomerRepository():new CustomerRepository();
 
     }
 }
